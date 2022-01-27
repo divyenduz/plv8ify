21c9b82c1907d500f9f1e4b13cb99b381a11d5ae
Divyendu Singh <divyendu.z@gmail.com>
2022-01-27 12:06:07 +0100

    feat: read params dynamically

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 const esbuild = require('esbuild')
 const fs = require('fs')
 
+const { Project } = require('ts-morph')
+
+const typeMap = {
+    'number': 'float8'
+}
+
 async function main() {
     esbuild.build({
-        entryPoints: ['input.js'],
+        entryPoints: ['input.ts'],
         bundle: true,
         platform: 'node',
         outfile: 'output.js',
@@ -13,10 +19,35 @@ async function main() {
 
     let bundledJs = fs.readFileSync('./output.js', 'utf8')
     bundledJs = bundledJs.replace('console.log(main());', 'return main()')
+
+    const project = new Project()
+    const sourceFile = project.addSourceFileAtPath("input.ts");
+
+    const ast = sourceFile.getFunction('main')
+    const name = ast.getName()
+    const params = ast.getParameters()
+
+    const paramsBind = params
+        .map(p => {
+            const name = p.getName()
+            const type = p.getType().getText()
+            const mappedType = typeMap[type]
+            return `${name} ${mappedType}`
+        }).join(',')
+
+    const paramsCall = params
+        .map(p => {
+            const name = p.getName()
+            return `${name}`
+        }).join(',')
+
     const plv8FunctionShell = `
-DROP FUNCTION plv8_test();
-CREATE OR REPLACE FUNCTION plv8_test() RETURNS JSON AS $$
+DROP FUNCTION IF EXISTS ${name}(${paramsBind});
+CREATE OR REPLACE FUNCTION ${name}(${paramsBind}) RETURNS JSON AS $$
     ${bundledJs}
+
+    return ${name}(${paramsCall})
+
 $$ LANGUAGE plv8 IMMUTABLE STRICT;
     `
     console.log(plv8FunctionShell)
