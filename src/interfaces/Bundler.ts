c53e0d766ff8d70659372787b77db75610e2ee47
Divyendu Singh <divyendu.z@gmail.com>
2022-10-31 10:05:13 +0100

    chore: comments

--- src/interfaces/Bundler.ts
+++ src/interfaces/Bundler.ts
@@ -1,6 +1,6 @@
 interface BundleArgs {
   inputFile: string
-  globalName: string
+  globalName: string // TODO: fixme, this is a part of esbuild config, ideally this shouldn't be a part of the common interface (unless all bundlers use this)
 }
 
 interface Bundler {
