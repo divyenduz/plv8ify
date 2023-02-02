97ca48a107d383936fd95355cd3e8b7f1f4b6804
Divyendu Singh <divyendu.z@gmail.com>
2023-02-02 13:12:35 +0100

    refactor: move argument parsing to a separate file

---
+++ src/impl/ParseCLI.ts
@@ -0,0 +1,51 @@
+import arg from 'arg'
+
+type Command = 'version' | 'generate'
+
+export class ParseCLI {
+  static getCommand() {
+    // CLI Args
+    // TODO: scope config options to commands
+    const args = arg({
+      '--input-file': String,
+      '--output-folder': String,
+      '--write-bundler-output': Boolean,
+      '--scope-prefix': String,
+      '--pg-function-delimiter': String,
+      '--fallback-type': String,
+      '--mode': String,
+      '--volatility': String,
+      '--debug': Boolean,
+    })
+
+    if (args._.length === 0) {
+      throw new Error(
+        `Please specify a command. Available commands: generate, version`
+      )
+    }
+
+    const inputFilePath = args['--input-file'] || 'input.ts'
+    const outputFolderPath = args['--output-folder'] || 'plv8ify-dist'
+    const writeBundlerOutput = args['--write-bundler-output'] || false
+    const scopePrefix = args['--scope-prefix'] || 'plv8ify'
+    const pgFunctionDelimiter = args['--pg-function-delimiter'] || '$plv8ify$'
+    const fallbackReturnType = args['--fallback-type'] || 'JSONB'
+    const mode = (args['--mode'] || 'inline') as Mode
+    const defaultVolatility = (args['--volatility'] ||
+      'IMMUTABLE') as Volatility
+
+    return {
+      command: args._[0] as Command,
+      config: {
+        writeBundlerOutput,
+        inputFilePath,
+        outputFolderPath,
+        scopePrefix,
+        pgFunctionDelimiter,
+        fallbackReturnType,
+        mode,
+        defaultVolatility,
+      },
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,66 @@
 #!/usr/bin/env node
-import arg from 'arg'
 import fs from 'fs'
 import 'reflect-metadata'
+import { match } from 'ts-pattern'
 
+import { ParseCLI } from './impl/ParseCLI'
 import TYPES from './interfaces/types'
 import container from './inversify.config'
 
 async function main() {
-  // CLI Args
-  const args = arg({
-    '--write-bundler-output': Boolean,
-    '--input-file': String,
-    '--output-folder': String,
-    '--scope-prefix': String,
-    '--pg-function-delimiter': String,
-    '--fallback-type': String,
-    '--mode': String,
-    '--volatility': String,
-    '--debug': Boolean,
-  })
+  const CLI = ParseCLI.getCommand()
 
-  if (args._.length === 0) {
-    throw new Error(
-      `Please specify a command. Available commands: generate, version`
-    )
-  }
-
-  if (args._[0] === 'version') {
+  if (CLI.command === 'version') {
     const pkg = require('../package.json')
     console.log(`Version: ${pkg.version}`)
     process.exit(0)
   }
 
-  const writeBundlerOutput = args['--write-bundler-output'] || false
-  const inputFilePath = args['--input-file'] || 'input.ts'
-  const outputFolderPath = args['--output-folder'] || 'plv8ify-dist'
-  const scopePrefix = args['--scope-prefix'] || 'plv8ify'
-  const pgFunctionDelimiter = args['--pg-function-delimiter'] || '$plv8ify$'
-  const fallbackReturnType = args['--fallback-type'] || 'JSONB'
-  const mode = (args['--mode'] || 'inline') as Mode
-  const defaultVolatility = (args['--volatility'] || 'IMMUTABLE') as Volatility
+  const _ = match(CLI.command)
+    .with('generate', async () => {
+      const {
+        writeBundlerOutput,
+        inputFilePath,
+        outputFolderPath,
+        scopePrefix,
+        pgFunctionDelimiter,
+        fallbackReturnType,
+        mode,
+        defaultVolatility,
+      } = CLI.config
 
-  fs.mkdirSync(outputFolderPath, { recursive: true })
+      fs.mkdirSync(outputFolderPath, { recursive: true })
 
-  const plv8ify = container.get<PLV8ify>(TYPES.PLV8ify)
-  plv8ify.init(inputFilePath)
+      const plv8ify = container.get<PLV8ify>(TYPES.PLV8ify)
+      plv8ify.init(inputFilePath)
 
-  const bundledJs = await plv8ify.build({
-    mode,
-    inputFile: inputFilePath,
-    scopePrefix: 'plv8ify',
-  })
+      const bundledJs = await plv8ify.build({
+        mode,
+        inputFile: inputFilePath,
+        scopePrefix: 'plv8ify',
+      })
 
-  // Optionally, write ESBuild output file
-  if (writeBundlerOutput) {
-    plv8ify.write(`${outputFolderPath}/output.js`, bundledJs)
-  }
+      // Optionally, write ESBuild output file
+      if (writeBundlerOutput) {
+        plv8ify.write(`${outputFolderPath}/output.js`, bundledJs)
+      }
 
-  // Emit SQL files for each exported function in the input TS file
-  const sqlFiles = plv8ify.getPLV8SQLFunctions({
-    mode,
-    scopePrefix,
-    defaultVolatility,
-    bundledJs,
-    pgFunctionDelimiter,
-    fallbackReturnType,
-    outputFolder: outputFolderPath,
-  })
+      // Emit SQL files for each exported function in the input TS file
+      const sqlFiles = plv8ify.getPLV8SQLFunctions({
+        mode,
+        scopePrefix,
+        defaultVolatility,
+        bundledJs,
+        pgFunctionDelimiter,
+        fallbackReturnType,
+        outputFolder: outputFolderPath,
+      })
 
-  sqlFiles.forEach((sqlFile) => {
-    plv8ify.write(sqlFile.filename, sqlFile.sql)
-  })
+      sqlFiles.forEach((sqlFile) => {
+        plv8ify.write(sqlFile.filename, sqlFile.sql)
+      })
+    })
+    .exhaustive()
 }
 
 main()
