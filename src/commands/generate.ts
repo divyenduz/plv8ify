aa27ea5b3e65c28b76963927c3f1307aafbf79aa
Divyendu Singh <mail@divyendusingh.com>
2023-03-04 18:57:35 +0100

    fix: prod build, add example of mathjs

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
-import { ParseCLI } from 'src/impl/ParseCLI'
-import TYPES from 'src/interfaces/types'
 
+import { ParseCLI } from '../impl/ParseCLI'
+import TYPES from '../interfaces/types'
 import container from '../inversify.config'
 
 export async function generateCommand(
