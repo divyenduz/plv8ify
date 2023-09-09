31b88e81eeb0b9b84903da79c33f725972ce9657
Divyendu Singh <mail@divyendusingh.com>
2023-09-09 19:34:23 +0200

    refactor: use esm and remove export instead of cjs for generated functions

--- examples/hello-start_proc/plv8ify-dist/plv8ify_hello.plv8.sql
+++ examples/hello-start_proc/plv8ify-dist/plv8ify_hello.plv8.sql
@@ -1,6 +1,6 @@
 DROP FUNCTION IF EXISTS plv8ify_hello();
 CREATE OR REPLACE FUNCTION plv8ify_hello() RETURNS text AS $plv8ify$
 
-return plv8ify.hello()
+return hello()
 
 $plv8ify$ LANGUAGE plv8 IMMUTABLE STRICT;
