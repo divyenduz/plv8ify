691eb0070e2e1b8c52bd7b0100bc2ce59a1b3b46
Divyendu Singh <mail@divyendusingh.com>
2023-09-10 09:45:46 +0200

    fix(version): esm + require

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,3 +1,7 @@
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
 export function versionCommand() {
   const pkg = require('../../package.json')
   console.log(`Version: ${pkg.version}`)
