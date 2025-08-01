fb24bef6f5e35e6d8c8d2d109e617c26a19e7652
Divyendu Singh <mail@divyendusingh.com>
2025-08-01 23:02:58 +0200

    add terragon-setup.sh

--- terragon-setup.sh
+++ terragon-setup.sh
@@ -10,5 +10,11 @@ env
 echo ~/
 export PATH="~/.config/xata/bin:$PATH"
 
+xata version
 xata status
-xata checkout main
+xata branch delete $E2B_SANDBOX_ID --yes || true
+# Create also does branch checkout
+xata branch create --name $E2B_SANDBOX_ID
+unset XATA_BRANCHID
+xata branch wait-ready $E2B_SANDBOX_ID
+echo DATABASE_URL="$(xata branch view $E2B_SANDBOX_ID --json | jq -r '.connectionString')" > .env
