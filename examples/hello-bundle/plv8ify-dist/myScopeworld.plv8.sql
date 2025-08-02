ac4ad86303369d41a7a4346eb8b31303f3d16a2d
Divyendu Singh <mail@divyendusingh.com>
2025-08-02 11:08:25 +0200

    update examples

--- examples/hello-bundle/plv8ify-dist/myScopeworld.plv8.sql
+++ examples/hello-bundle/plv8ify-dist/myScopeworld.plv8.sql
@@ -1,6 +1,6 @@
 DROP FUNCTION IF EXISTS myScopeworld();
 CREATE OR REPLACE FUNCTION myScopeworld() RETURNS text AS $plv8ify$
-if (!globalThis[Symbol.for('myScope_initialized')]) plv8.execute('SELECT myScope_init();');
+if (!globalThis[Symbol.for('myScope_initialized')]!== true) plv8.execute('SELECT myScope_init();');
 return world()
 
 $plv8ify$ LANGUAGE plv8 IMMUTABLE STRICT;
